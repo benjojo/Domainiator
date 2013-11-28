0ea1a60fb4ba391605eb16a4554ba924184b402a
Ben Cox <ben@benjojo.co.uk>
2013-11-28 13:36:22 +0000

    Cleaned up the file a tad more

--- Crawl.go
+++ Crawl.go
@@ -30,10 +30,6 @@ func main() {
 		panic(e)
 	}
 	File := strings.Split(string(b), "\n")
-	// yourLinksSlice := make([]string, 50)
-	// for i := 0; i < 50; i++ {
-	// 	yourLinksSlice[i] = fmt.Sprintf("%d", i+1)
-	// }
 
 	lCh := make(chan string)
 	wg := new(sync.WaitGroup)
@@ -44,15 +40,11 @@ func main() {
 		go worker(lCh, wg)
 	}
 
-	// Processing all links by spreading them to `free` goroutines
 	for _, link := range File {
 		lCh <- link
 	}
-
 	// Closing channel (waiting in goroutines won't continue any more)
 	close(lCh)
-
-	// Waiting for all goroutines to finish (otherwise they die as main routine dies)
 	wg.Wait()
 }
 
