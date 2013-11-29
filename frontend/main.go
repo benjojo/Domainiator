acbc3ed8352928cbb56bf835666ba44c2b27cc05
Ben Cox <Ben@Benjojo.co.uk>
2013-11-29 22:14:48 +0000

    A functioning ground zero

--- frontend/main.go
+++ frontend/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/martini"
+	"log"
 	"net/http"
 )
 
 func main() {
-
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	// Test if the database works.
+	Database, e := GetDB()
+	if e != nil {
+		panic(e)
+	}
+	// Okay so now we have a database connection.
+	m := martini.Classic()
+	m.Run()
 }
