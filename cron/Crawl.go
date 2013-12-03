54a8c792126ac0e876f1584a2c76f3574cf3e880
Ben Cox <Ben@Benjojo.co.uk>
2013-12-03 22:45:50 +0000

    Add the DB runner

--- cron/Crawl.go
+++ cron/Crawl.go
@@ -5,6 +5,24 @@ import (
 )
 
 func main() {
-	database := GetDB()
+	fmt.Println("Attemping to connect to DB")
+	database, _ := GetDB()
+	// Okay so we are gonna grab a few really basic stats here.
 
+	// So the first bit we are going to get is the total done today.
+	// SELECT COUNT(*) FROM Results WHERE `Timestamp` > timestampadd(hour, -24, now())
+	var Total int
+
+	database.QueryRow("SELECT COUNT(*) FROM Results WHERE `Data` != 'f'").Scan(&Total)
+	// This one takes approx 10 mins :eek:
+
+	var TotalFailed int
+	database.QueryRow("SELECT COUNT(*) FROM Results WHERE `Data` = 'f'").Scan(&TotalFailed)
+
+	// TODO: make it process the headers and get the avg content length.
+
+	database.Exec("INSERT INTO `Domaniator`.`CachedResults` (`RequestCount`, `FailedCount`) VALUES (?, ?);", Total, TotalFailed)
+
+	database.Close()
+	fmt.Println("Done.")
 }
