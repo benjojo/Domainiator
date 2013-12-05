9d161980d47ab9ce41de65fa972ec3aa1995eb66
Ben Cox <Ben@Benjojo.co.uk>
2013-12-05 21:53:43 +0000

    Adding a run timer on the crawler

--- cron/Crawl.go
+++ cron/Crawl.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Attemping to connect to DB")
 	database, _ := GetDB()
 	// Okay so we are gonna grab a few really basic stats here.
-
+	start := time.Now()
 	// So the first bit we are going to get is the total done today.
 	// SELECT COUNT(*) FROM Results WHERE `Timestamp` > timestampadd(hour, -24, now())
 	var Total int
@@ -24,5 +25,6 @@ func main() {
 	database.Exec("INSERT INTO `Domaniator`.`CachedResults` (`RequestCount`, `FailedCount`) VALUES (?, ?);", Total, TotalFailed)
 
 	database.Close()
-	fmt.Println("Done.")
+	elapsed := time.Since(start)
+	fmt.Println("Done in %s", elapsed)
 }
