60f1ba468881eba70bbe703f446dff15b81eb9c4
Ben Cox <Ben@Benjojo.co.uk>
2013-12-28 18:34:10 +0000

    Get row counts

--- analytics/main.go
+++ analytics/main.go
@@ -6,6 +6,11 @@ import (
 
 func main() {
 	fmt.Println("DB analyitcs started -- Connecting to Database")
-	database := GetDB()
-
+	database, e := GetDB()
+	if e != nil {
+		fmt.Errorf("Oh dear, I was unable to connect to the database for this reason %s\n I will exit now.", e.Error())
+		return
+	}
+	var rowcount int
+	database.QueryRow("SELECT COUNT(*) FROM `Results` WHERE `Data` != 'f'").Scan(&rowcount)
 }
