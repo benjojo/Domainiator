4a390cb4c12fc0960c78357ebae40a0ab4723636
Ben Cox <ben@benjojo.co.uk>
2014-02-03 00:41:21 +0000

    Prevented a nil pointer wrecking ball

--- database.go
+++ database.go
@@ -10,6 +10,7 @@ func GetDB() (con *sql.DB, e error) {
 	con, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/Domaniator")
 	if err != nil {
 		fmt.Println("[DB] An error happened in the setup of a SQL connection")
+		return con, err
 	}
 	con.Ping()
 	con.Exec("SET NAMES UTF8")
