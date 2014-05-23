ea59b0de1082ad9103ec3e11f69b87b66067f1bc
Ben Cox <Ben@Benjojo.co.uk>
2014-05-23 23:22:50 +0100

    Added a way to change how big the buffer is

--- Crawl.go
+++ Crawl.go
@@ -143,6 +143,7 @@ func main() {
 	concurrencycount := flag.Int("concount", 600, "How many go routines you want to start")
 	databasestring = flag.String("dbstring", "root:@tcp(127.0.0.1:3306)/Domaniator", "What to connect to the database with")
 	useragent = flag.String("ua", "HTTP Header Survey By Benjojo +https://github.com/benjojo/Domainiator", "What UA to send the request with")
+	logbuffer = flag.Int("logbuffer", 100, "How many logging entries should be buffered before blocking")
 
 	flag.Parse()
 
@@ -158,7 +159,7 @@ func main() {
 	File := strings.Split(string(b), "\n")
 
 	lCh := make(chan string)
-	rCh := make(chan LogPayload, 100)
+	rCh := make(chan LogPayload, *logbuffer)
 	wg := new(sync.WaitGroup)
 	go Logger(rCh)
 	// Adding routines to workgroup and running then
